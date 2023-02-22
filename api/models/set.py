9288a01c2cfac32fcb49b07a8236cb579e70bd05
provideforme <evan.a.bernard@gmail.com>
2023-02-22 16:40:53 -0500

    fix set

--- api/models/set.py
+++ api/models/set.py
@@ -4,15 +4,15 @@ from api.models.db import db
 class Set(db.Model):
   __tablename__ = 'sets'
   id = db.Column(db.Integer, primary_key=True)
-  brand = db.Column(db.String(100))
+  name = db.Column(db.String(100))
   series = db.Column(db.String(100))
   year = db.Column(db.Integer)
   created_at = db.Column(db.DateTime, default=datetime.utcnow)
   profile_id = db.Column(db.Integer, db.ForeignKey('profiles.id'))
 
   def __repr__(self):
-    return f"Set('{self.id}', '{self.brand}'"
+    return f"Set('{self.id}', '{self.name}'"
 
   def serialize(self):
-    set = {c.brand: getattr(self, c.brand) for c in self.__table__.columns}
+    set = {c.name: getattr(self, c.name) for c in self.__table__.columns}
     return set
