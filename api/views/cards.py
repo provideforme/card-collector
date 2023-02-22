6acd2f5ac659b049192dab76de1248f28c2ec4f8
provideforme <evan.a.bernard@gmail.com>
2023-02-22 10:34:47 -0500

    create card

--- api/views/cards.py
+++ api/views/cards.py
@@ -5,3 +5,14 @@ from api.models.db import db
 from api.models.card import Card
 
 cards = Blueprint('cards', 'cards')
+
+@cards.route('/', methods=["POST"])
+@login_required
+def create():
+  data = request.get_json()
+  profile = read_token(request)
+  data["profile_id"] = profile["id"]
+  card = Card(**data)
+  db.session.add(card)
+  db.session.commit()
+  return jsonify(card.serialize()), 201
